272168874791aeda8d77bd3e52fc1d9317759315
cryanbrow <cryan.brow@gmail.com>
2022-02-27 19:07:41 -0600

    Working on data for solar system

--- model/solar_system.go
+++ model/solar_system.go
@@ -1,8 +1,18 @@
 package model
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+var sdeSolarSystem solarSystem
+
 type solarSystem map[int]struct {
 	Border                     bool             `yaml:"border" json:"border"`
-	Center                     []int            `yaml:"center" json:"center"`
+	Center                     []float64        `yaml:"center" json:"center"`
 	ConstellationID            int              `json:"constellationID"`
 	Corridor                   bool             `yaml:"corridor" json:"corridor"`
 	DisallowedAnchorCategories []int            `yaml:"disallowedAnchorCategories" json:"disallowedAnchorCategories"`
@@ -10,12 +20,13 @@ type solarSystem map[int]struct {
 	Fringe                     bool             `yaml:"fringe" json:"fringe"`
 	Hub                        bool             `yaml:"hub" json:"hub"`
 	International              bool             `yaml:"international" json:"international"`
-	Luminosity                 float32          `yaml:"luminosity" json:"luminosity"`
+	Luminosity                 float64          `yaml:"luminosity" json:"luminosity"`
 	Max                        []float64        `yaml:"max" json:"max"`
 	Min                        []float64        `yaml:"min" json:"min"`
 	Planets                    map[int]planet   `yaml:"planets" json:"planets"`
 	Radius                     float64          `yaml:"radius" json:"radius"`
 	Regional                   bool             `yaml:"regional" json:"regional"`
+	RegionID                   int              `json:"regionID"`
 	Security                   float64          `yaml:"security" json:"security"`
 	SecurityClass              string           `yaml:"securityClass" json:"securityClass"`
 	SolarSystemID              int              `yaml:"solarSystemID" json:"solarSystemID"`
@@ -33,21 +44,21 @@ type asteroidBelt struct {
 
 type statisticsType struct {
 	Density        float32 `yaml:"density" json:"density"`
-	Eccentricity   string  `yaml:"eccentricity" json:"eccentricity"`
-	EscapeVelocity float32 `yaml:"escapeVelocity" json:"escapeVelocity"`
+	Eccentricity   float64 `yaml:"eccentricity" json:"eccentricity"`
+	EscapeVelocity float64 `yaml:"escapeVelocity" json:"escapeVelocity"`
 	Fragmented     bool    `yaml:"fragmented" json:"fragmented"`
-	Life           float32 `yaml:"life" json:"life"`
+	Life           float64 `yaml:"life" json:"life"`
 	Locked         bool    `yaml:"locked" json:"locked"`
-	MassDust       string  `yaml:"massDust" json:"massDust"`
-	MassGas        string  `yaml:"massGas" json:"massGas"`
-	OrbitPeriod    float32 `yaml:"orbitPeriod" json:"orbitPeriod"`
-	OrbitRadius    float32 `yaml:"orbitRadius" json:"orbitRadius"`
-	Pressure       string  `yaml:"pressure" json:"pressure"`
-	Radius         float32 `yaml:"radius" json:"radius"`
-	RotationRate   float32 `yaml:"rotationRate" json:"rotationRate"`
+	MassDust       float64 `yaml:"massDust" json:"massDust"`
+	MassGas        float64 `yaml:"massGas" json:"massGas"`
+	OrbitPeriod    float64 `yaml:"orbitPeriod" json:"orbitPeriod"`
+	OrbitRadius    float64 `yaml:"orbitRadius" json:"orbitRadius"`
+	Pressure       float64 `yaml:"pressure" json:"pressure"`
+	Radius         float64 `yaml:"radius" json:"radius"`
+	RotationRate   float64 `yaml:"rotationRate" json:"rotationRate"`
 	SpectralClass  string  `yaml:"spectralClass" json:"spectralClass"`
-	SurfaceGravity float32 `yaml:"surfaceGravity" json:"surfaceGravity"`
-	Temperature    float32 `yaml:"temperature" json:"temperature"`
+	SurfaceGravity float64 `yaml:"surfaceGravity" json:"surfaceGravity"`
+	Temperature    float64 `yaml:"temperature" json:"temperature"`
 }
 
 type starStatisticsType struct {
@@ -79,7 +90,7 @@ type planetAttributesType struct {
 }
 
 type moon struct {
-	npcStations      map[int]npcStation
+	NPCStations      map[int]npcStation
 	PlanetAttributes planetAttributesType `yaml:"planetAttributes" json:"planetAttributes"`
 	Position         []float64            `yaml:"position" json:"position"`
 	Padius           int                  `yaml:"radius" json:"radius"`
@@ -88,16 +99,16 @@ type moon struct {
 }
 
 type npcStation struct {
-	graphicID                int       `yaml:"graphicID" json:"graphicID"`
-	isConquerable            bool      `yaml:"isConquerable" json:"isConquerable"`
-	operationID              int       `yaml:"operationID" json:"operationID"`
-	ownerID                  int       `yaml:"ownerID" json:"ownerID"`
-	position                 []float64 `yaml:"position" json:"position"`
-	reprocessingEfficiency   float32   `yaml:"reprocessingEfficiency" json:"reprocessingEfficiency"`
-	reprocessingHangarFlag   int       `yaml:"reprocessingHangarFlag" json:"reprocessingHangarFlag"`
-	reprocessingStationsTake float32   `yaml:"reprocessingStationsTake" json:"reprocessingStationsTake"`
-	typeID                   int       `yaml:"typeID" json:"typeID"`
-	useOperationName         bool      `yaml:"useOperationName" json:"useOperationName"`
+	GraphicID                int       `yaml:"graphicID" json:"graphicID"`
+	IsConquerable            bool      `yaml:"isConquerable" json:"isConquerable"`
+	OperationID              int       `yaml:"operationID" json:"operationID"`
+	OwnerID                  int       `yaml:"ownerID" json:"ownerID"`
+	Position                 []float64 `yaml:"position" json:"position"`
+	ReprocessingEfficiency   float32   `yaml:"reprocessingEfficiency" json:"reprocessingEfficiency"`
+	ReprocessingHangarFlag   int       `yaml:"reprocessingHangarFlag" json:"reprocessingHangarFlag"`
+	ReprocessingStationsTake float32   `yaml:"reprocessingStationsTake" json:"reprocessingStationsTake"`
+	TypeID                   int       `yaml:"typeID" json:"typeID"`
+	UseOperationName         bool      `yaml:"useOperationName" json:"useOperationName"`
 }
 
 type starType struct {
@@ -111,4 +122,22 @@ type stargate struct {
 	Destination int       `yaml:"destination" json:"destination"`
 	Position    []float64 `yaml:"position" json:"position"`
 	TypeID      int       `yaml:"typeID" json:"typeID"`
+}
+
+func LoadSolarSystem(path string) {
+	var file *os.File
+	var err error
+	file, _ = os.Open(filepath.Clean(path))
+	defer func() {
+		if err := file.Close(); err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&sdeSolarSystem)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
